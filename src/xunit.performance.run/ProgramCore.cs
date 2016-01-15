f0c6cdaf43777a87f9cedb7a66146e39a14d3f7a
Victor Sia <visia@microsoft.com>
2016-01-14 16:22:35 -0800

    Set iterations back to 2

--- src/xunit.performance.run/ProgramCore.cs
+++ src/xunit.performance.run/ProgramCore.cs
@@ -137,8 +137,8 @@ namespace Microsoft.Xunit.Performance
             };
 
             startInfo.Environment["XUNIT_PERFORMANCE_RUN_ID"] = project.RunId;
-            startInfo.Environment["XUNIT_PERFORMANCE_MIN_ITERATION"] = "0";
-            startInfo.Environment["XUNIT_PERFORMANCE_MAX_ITERATION"] = "0";
+            startInfo.Environment["XUNIT_PERFORMANCE_MIN_ITERATION"] = "1";
+            startInfo.Environment["XUNIT_PERFORMANCE_MAX_ITERATION"] = "1";
             startInfo.Environment["XUNIT_PERFORMANCE_MAX_TOTAL_MILLISECONDS"] = "1000";
             startInfo.Environment["COMPLUS_gcConcurrent"] = "0";
             startInfo.Environment["COMPLUS_gcServer"] = "0";
